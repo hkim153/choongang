a5ee2f2c650c778ab9f3d72388ea0060993597bb
hkim153 <harry100429@hanmail.net>
2019-10-21 20:40:19 +0900

    19/10/21 last by hyunsoo

--- project1/src/dao/fishingsite.java
+++ project1/src/dao/fishingsite.java
@@ -1,5 +1,68 @@
 package dao;
 
 public class fishingsite {
-
+	private String fs_code;
+	private String e_code;
+	private String f_code;
+	private int num;
+	private String fs_name;
+	private String fs_addr;
+	private String fs_reg;
+	private String fs_content;
+	private String fs_img_url;
+	public String getFs_code() {
+		return fs_code;
+	}
+	public void setFs_code(String fs_code) {
+		this.fs_code = fs_code;
+	}
+	public String getE_code() {
+		return e_code;
+	}
+	public void setE_code(String e_code) {
+		this.e_code = e_code;
+	}
+	public String getF_code() {
+		return f_code;
+	}
+	public void setF_code(String f_code) {
+		this.f_code = f_code;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getFs_name() {
+		return fs_name;
+	}
+	public void setFs_name(String fs_name) {
+		this.fs_name = fs_name;
+	}
+	public String getFs_addr() {
+		return fs_addr;
+	}
+	public void setFs_addr(String fs_addr) {
+		this.fs_addr = fs_addr;
+	}
+	public String getFs_reg() {
+		return fs_reg;
+	}
+	public void setFs_reg(String fs_reg) {
+		this.fs_reg = fs_reg;
+	}
+	public String getFs_content() {
+		return fs_content;
+	}
+	public void setFs_content(String fs_content) {
+		this.fs_content = fs_content;
+	}
+	public String getFs_img_url() {
+		return fs_img_url;
+	}
+	public void setFs_img_url(String fs_img_url) {
+		this.fs_img_url = fs_img_url;
+	}
+	
 }
